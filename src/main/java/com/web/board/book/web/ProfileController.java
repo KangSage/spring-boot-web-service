eb643947f16d52a0e9f72ee855b01b5acdee1da8
KangSage <ksage@knou.ac.kr>
2020-07-19 02:43:12 +0900

    add profile controller's log

--- src/main/java/com/web/board/book/web/ProfileController.java
+++ src/main/java/com/web/board/book/web/ProfileController.java
@@ -1,6 +1,7 @@
 package com.web.board.book.web;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Arrays;
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class ProfileController {
@@ -18,6 +20,7 @@ public class ProfileController {
         List<String> profiles = Arrays.asList(env.getActiveProfiles());
         List<String> realProfiles = Arrays.asList("real", "real1", "real2");
         String defaultProfile = profiles.isEmpty() ? "default" : profiles.get(0);
+        log.info("현재 프로파일: {}", defaultProfile);
         return profiles.stream()
                 .filter(realProfiles::contains)
                 .findAny()
